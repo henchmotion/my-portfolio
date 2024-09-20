7cea290eaec28c0b0436c9495ff7dde978a11353
Henry Erondu <henchmotion@gmail.com>
2024-09-20 14:27:05 +0100

    Updates in styles ansd pages

--- js/toggle.js
+++ js/toggle.js
@@ -1,12 +1,3 @@
-
-let typingEffect = new Typed(".name", {
-    strings: ["Henry Erondu", " A Frontend Developer"," A Game Developer", " An Administartor"],
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 1500,
-})
-
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
@@ -14,4 +5,3 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
-
